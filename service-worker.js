aec19295ef073eea2a81082cb95b6d5ef25e94b1
didatronic <rildoribeiro20@gmail.com>
2026-02-12 15:17:53 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,31 @@
-const CACHE_NAME = 'technivel-v1';
+const CACHE_NAME = 'technivel-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js',
   'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&display=swap'
 ];
 
-// Instala o Service Worker e faz cache dos arquivos
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Arquivos em cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Responde as requisições com o cache ou busca na rede
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
         if (response) {
-          return response; // Retorna do cache se existir
+          return response;
         }
-        return fetch(event.request); // Se não, busca na internet
+        return fetch(event.request);
       })
   );
 });
